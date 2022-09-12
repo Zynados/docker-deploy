298d9afa135ea437d8fa926277401bf34e95fa26
zikeji <me@zikeji.com>
2022-09-12 04:07:20 -0400

    Fixed missing sudo calls

--- install.sh
+++ install.sh
@@ -40,11 +40,11 @@ echo "Installing Docker."
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update -y
-apt-get install -y docker-ce docker-compose
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ${USER}
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-compose
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
 
 echo "Installing required packages."
 sudo apt-get install -y jq nano backblaze-b2
